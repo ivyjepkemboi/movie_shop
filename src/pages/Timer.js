99fb45926aaab554903ec14b0a47fc461647cc59
Ivy <ivyjkemboi@gmail.com>
2024-06-05 18:33:19 +0300

    ádded form to timer

--- src/pages/Timer.js
+++ src/pages/Timer.js
@@ -17,7 +17,6 @@ useTitle('countdown')
         }
       });
     }, 1000);
-
     // Cleanup function to clear the interval when the component unmounts or when seconds reach 0
     return () => clearInterval(timer);
   }, []); // Run effect only once after initial render
@@ -30,7 +29,21 @@ useTitle('countdown')
   return (
     <div className="p-4 h-screen">
       <div className="bg-gray-200 p-6 rounded-lg">
-        <h1 className="text-3xl font-bold mb-4 text-center">Pay half price if you make a payment before this countdown ends!!!</h1>
+        
+
+<form class="max-w-sm mx-auto">
+  <div class="mb-5">
+    <label for="email" class="block mb-2 text-sm font-medium">Your Card Details</label>
+    <input class="text-sm rounded-lg  block w-full p-2.5 "  />
+  </div>
+  <div class="mb-5">
+    <label for="email" class="block mb-2 text-sm font-medium">Card Holder Name</label>
+    <input class="text-sm rounded-lg  block w-full p-2.5 "  />
+  </div>
+    <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Click to pay</button>
+</form>
+
+        <h1 className="text-3xl font-bold mb-4 text-center">Pay half price if you make a payment within one minute!!!</h1>
         <div className="text-6xl font-bold text-center">{seconds}</div>
       </div>
     </div>
